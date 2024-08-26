2441e826acc537b5f6482a3fa513c64e32f5835b
Neeraj Rana <neeraj@getcovered.io>
2024-08-26 21:32:21 +0530

    added empty string functionality

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,2 +1,5 @@
 class StringCalculator
+  def self.add(input_str)
+    return 0 if input_str.empty?
+  end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,5 +2,11 @@ require 'rspec'
 require './lib/string_calculator'
 describe "It should calculate sum of the number in a string" do
 
+  context "Checks for positive numbers and other characters" do
+    it "should return 0 if empty sting is provided" do
+      expect(StringCalculator.add("")).to eq(0)
+    end
+    
+  end
 
 end
