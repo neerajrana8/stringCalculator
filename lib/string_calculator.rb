d7d451a0266dab8af001d1a0e83eeffa99246e7e
Neeraj Rana <neeraj@getcovered.io>
2024-08-26 21:39:49 +0530

    added basic string sum

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,7 @@
 class StringCalculator
   def self.add(input_str)
     return 0 if input_str.empty?
+    input_array = input_str.split(',').map(&:to_i)
+    input_array.sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,10 +3,18 @@ require './lib/string_calculator'
 describe "It should calculate sum of the number in a string" do
 
   context "Checks for positive numbers and other characters" do
-    it "should return 0 if empty sting is provided" do
+    it "should return 0 if empty string is provided" do
       expect(StringCalculator.add("")).to eq(0)
     end
-    
+
+    it "should return 4 if 4 is provided" do
+      expect(StringCalculator.add("4")).to eq(4)
+    end
+
+    it "should return 10 if (4,6) is provided" do
+      expect(StringCalculator.add("4,6")).to eq(10)
+    end
+
   end
 
 end
